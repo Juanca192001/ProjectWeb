87a2e316f8a69cd194a8851f60c01c3890182d1d
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 16:59:20 +0200

    Update models.py

--- ProyectoWeb/models.py
+++ ProyectoWeb/models.py
@@ -1,14 +1,35 @@
 from django.db import models
 
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Modelo(models.Model):
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.marca} - {self.nombre}"
+
 class Coche(models.Model):
+    TIPO_CHOICES = [
+        ('SUV', 'SUV'),
+        ('Berlina', 'Berlina'),
+        ('Todo Terreno', 'Todo Terreno'),
+    ]
     RUEDAS_CHOICES = [('default', 'Default'), ('negro', 'Negro')]
     INTERIOR_CHOICES = [('default', 'Default'), ('cuero', 'Cuero')]
     COLOR_CHOICES = [('blanco', 'Blanco'), ('negro', 'Negro')]
 
-    nombre = models.CharField(max_length=100)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
     ruedas = models.CharField(max_length=10, choices=RUEDAS_CHOICES, default='default')
     interior = models.CharField(max_length=10, choices=INTERIOR_CHOICES, default='default')
     color = models.CharField(max_length=10, choices=COLOR_CHOICES, default='blanco')
 
     def __str__(self):
-        return self.nombre
+        return f"{self.marca} {self.modelo} ({self.tipo})"
+
