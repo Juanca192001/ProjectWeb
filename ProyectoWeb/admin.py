58574ec11db99408a959b6b8958fca8a8075934b
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 17:09:35 +0200

    Update admin.py

--- ProyectoWeb/admin.py
+++ ProyectoWeb/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
-from .models import Coche
+from .models import Marca, Modelo, Coche
+
+@admin.register(Marca)
+class MarcaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+@admin.register(Modelo)
+class ModeloAdmin(admin.ModelAdmin):
+    list_display = ('marca', 'nombre')
+    list_filter = ('marca',)
 
 @admin.register(Coche)
 class CocheAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'ruedas', 'interior', 'color')
-    search_fields = ('nombre',)
+    list_display = ('marca', 'modelo', 'tipo', 'ruedas', 'interior', 'color')
+    list_filter = ('marca', 'modelo', 'tipo')
+    search_fields = ('modelo__nombre',)  # Search by model name
+
