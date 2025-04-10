0bbff5882261ce224bb6e5440ec1bc809999b97f
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 16:52:08 +0200

    Update tests.py

--- ProyectoWeb/tests.py
+++ ProyectoWeb/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class CocheTestCase(TestCase):
+    def test_creacion_coche(self):
+        coche = Coche.objects.create(nombre="Test", ruedas="negro", interior="cuero", color="blanco")
+        self.assertEqual(coche.nombre, "Test")
+        self.assertEqual(coche.ruedas, "negro")
+        self.assertEqual(coche.interior, "cuero")
+        self.assertEqual(coche.color, "blanco")
