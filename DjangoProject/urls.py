f22381277b08c8197d936beee0bfc258e5ce8e44
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 16:54:01 +0200

    Update urls.py

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -1,6 +1,5 @@
 """
 URL configuration for DjangoProject project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
@@ -14,9 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from ProyectoWeb.views import home, login_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('login/', login_view, name='login'),
 ]
