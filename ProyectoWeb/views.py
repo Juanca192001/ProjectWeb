2628ca552e8e9f97eeb279dee9b078867c4f9e70
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 17:06:03 +0200

    Update views.py

--- ProyectoWeb/views.py
+++ ProyectoWeb/views.py
@@ -1,10 +1,11 @@
-
 from django.shortcuts import render
-from .models import Coche
+from .models import Coche, Marca, Modelo
 
 def home(request):
     coches = Coche.objects.all()
-    return render(request, 'ProyectoWeb/home.html', {'coches': coches})
+    marcas = Marca.objects.all()
+    modelos = Modelo.objects.all()
+    return render(request, 'ProyectoWeb/home.html', {'coches': coches, 'marcas': marcas, 'modelos': modelos})
 
 def login_view(request):
     return render(request, 'ProyectoWeb/login.html')
