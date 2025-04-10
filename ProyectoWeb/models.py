f82cafdcd8c904c54e37b9f20e7214ed14b5de62
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 16:52:23 +0200

    Update models.py

--- ProyectoWeb/models.py
+++ ProyectoWeb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Coche(models.Model):
+    RUEDAS_CHOICES = [('default', 'Default'), ('negro', 'Negro')]
+    INTERIOR_CHOICES = [('default', 'Default'), ('cuero', 'Cuero')]
+    COLOR_CHOICES = [('blanco', 'Blanco'), ('negro', 'Negro')]
+
+    nombre = models.CharField(max_length=100)
+    ruedas = models.CharField(max_length=10, choices=RUEDAS_CHOICES, default='default')
+    interior = models.CharField(max_length=10, choices=INTERIOR_CHOICES, default='default')
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES, default='blanco')
+
+    def __str__(self):
+        return self.nombre
