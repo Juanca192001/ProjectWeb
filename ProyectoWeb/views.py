10f7b57679755c827a88f819e92229b8f2ee4b32
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-10 16:51:44 +0200

    Update views.py

--- ProyectoWeb/views.py
+++ ProyectoWeb/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Coche
+
+def home(request):
+    coches = Coche.objects.all()
+    return render(request, 'ProyectoWeb/home.html', {'coches': coches})
+
+def login_view(request):
+    return render(request, 'ProyectoWeb/login.html')
