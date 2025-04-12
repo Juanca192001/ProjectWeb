64872919c9fb0402f49f85766e918705c290df47
Joan Carles Turegano <joancarlesturegano@gmail.com>
2025-04-12 18:19:38 +0200

    Prova

--- ProyectoWeb/views.py
+++ ProyectoWeb/views.py
@@ -5,7 +5,12 @@ def home(request):
     coches = Coche.objects.all()
     marcas = Marca.objects.all()
     modelos = Modelo.objects.all()
-    return render(request, 'ProyectoWeb/home.html', {'coches': coches, 'marcas': marcas, 'modelos': modelos})
+    context = {
+        'coches': coches,
+        'marcas': marcas,
+        'modelos': modelos
+    }
+    return render(request, 'ProyectoWeb/home.html',context)
 
 def login_view(request):
     return render(request, 'ProyectoWeb/login.html')
