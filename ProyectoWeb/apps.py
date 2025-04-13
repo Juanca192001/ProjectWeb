01edd07cc9c81870a218ca2e4fead613ef0e31b1
AbrARM017 <abrahamruizmoste2001@gmail.com>
2025-04-13 16:33:54 +0200

    Update apps.py

--- ProyectoWeb/apps.py
+++ ProyectoWeb/apps.py
@@ -1,15 +1,5 @@
 from django.apps import AppConfig
-INSTALLED_APPS = [
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
-        # Agrega aquí tu app, por ejemplo:
-        'ProyectoWeb',
-        'User',
-    ]
+
 class ProyectowebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ProyectoWeb'
